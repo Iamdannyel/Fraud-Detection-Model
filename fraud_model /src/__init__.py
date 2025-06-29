5f905166f9a69360ef634c0d61687c23b53fc3ff
Iamdannyel <53523383+Iamdannyel@users.noreply.github.com>
2025-06-29 18:02:37 +0100

    Delete fraud_model  directory

--- fraud_model /src/__init__.py
+++
@@ -1,7 +0,0 @@
-
-
-from src.model_functions import load_data, engineer_features, identify_repeat_perpetrator
-#import model_job
-
-
-__all__ = ["load_data", "engineer_features", "identify_repeat_perpetrator"]
